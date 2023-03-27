76d115b1cd3dc519206ea4544700921554ee20c0
Kiran Bisht <kiranb0609@gmail.com>
2023-03-27 15:59:15 +0530

    added env

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:movies_app/screens/login.dart';
 
 void main() async{
+  await dotenv.load();
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
